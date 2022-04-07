85be8e66e64d3d790e2563e53acae1d5d3e1732a
ggg <guimel_g@hotmail.com>
2022-04-07 12:12:46 -0500

    sds

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-//imports
+// imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
+
 const APP_SHELL = [
-    //'/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -24,50 +26,75 @@ const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     'css/animate.css',
-    'js/libs/jquery.js',
-
+    'js/libs/jquery.js'
 ];
 
+
+
 self.addEventListener('install', e => {
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
-        cache.addAll(APP_SHELL));
-    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
-        cache.addAll(APP_SHELL_INMUTABLE));
+
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
 
 
 
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
 
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
+
 
 self.addEventListener('activate', e => {
-    const respuesta = caches.keys().then(keys => {
 
-        keys.forEach(key => {
-            if (key !== STATIC_CACHE && key.includes('static')) {
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-        });
-    });
 
-    e.waitUntil(respuesta);
-
-});
-
-self.addEventListener('fetch', e => {
-    const respuesta = caches.match(e.rquest)
-        .then(res => {
-            if (res) {
-                return res;
-            } else {
-                return fetch(e.request).then(newRes => {
-                    return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
-                });
-
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
             }
 
         });
-    e.respondWith(respuesta);
+
+    });
+
+    e.waitUntil( respuesta );
 
 });
+
+
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
+});
+
 
